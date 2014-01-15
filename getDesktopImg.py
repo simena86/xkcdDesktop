1c242dc9e985d11bf6fcf84452be4bd83dd264ea
simena86 <simena86@gmail.com>
2014-01-15 10:06:06 +0100

    added readme

--- getDesktopImg.py
+++ getDesktopImg.py
@@ -9,19 +9,26 @@ from PIL import Image
 from lxml import etree, html
 import os
 
+url='http://www.xkcd.com'
+
 # get the comic and save it as png
 def getImage(imgPath):
-	url='http://www.xkcd.com'
 	page = html.fromstring(urllib.urlopen(url).read())
 	img=page.xpath('//div[@id="comic"]/img/@src')
 	img=img[0]
 	urllib.urlretrieve(img, imgPath+ 'todaysXkcd.png')
 
 def getMouseOverText():
-	url='http://www.xkcd.com'
 	page = html.fromstring(urllib.urlopen(url).read())
 	mText=page.xpath('//div[@id="comic"]/img/@title')[0]
 	return mText
+
+def getHeader():
+	page = html.fromstring(urllib.urlopen(url).read())
+	header=page.xpath('//div[@id="ctitle"]/text()')[0]
+	print header
+	return header
+
 	
 # using pil for image
 from PIL import Image
@@ -29,34 +36,37 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 import textwrap
 
-def textToImage(text,imgPath):
-	xkcd=Image.open(imgPath)	
+def textToImage(text,imgPath,fileName):
+	fontPath = imgPath + "FreeMono.ttf"
+	font = ImageFont.truetype(fontPath, 14)
+	textW,textH=font.getsize("S")
+	xkcd=Image.open(imgPath + fileName)	
 	xwidth, xheight = xkcd.size
-	img= Image.new("RGBA", (xwidth,188), (255,255,255))
+	img= Image.new("RGBA", (xwidth,588), (255,255,255))
 	draw=ImageDraw.Draw(img)
-	font = ImageFont.truetype("FreeSerif.ttf", 14)
-	tcolor=(0,0,0)
-	margin = offset = xwidth/35
-	for line in textwrap.wrap(text, width=40):
-		draw.text((margin, offset), line, font=font, fill=tcolor)
+	margin = offset = 5
+	for line in textwrap.wrap(text, width=xwidth/textW-4):
+		draw.text((margin, offset), line, font=font, fill=(0,0,0))
 		offset += font.getsize(line)[1]
-	img.save('text.png')
+	img=img.crop((0,0,xwidth,offset+textH ))
+	return img
 
-def stitchImagesTogether(xkcdImgPath,textImgPath):
-	tImg=Image.open(textImgPath)
+def stitchImagesTogether(xkcdImgPath,tImg):
 	xImg=Image.open(xkcdImgPath)
 	xw,xh=xImg.size
 	tw,th=tImg.size
 	img= Image.new("RGBA", (xw,xh+th), (255,255,255))
 	img.paste(xImg,(0,0))
 	img.paste(tImg,(0,xh))
-	img.save('todaysXkcd.png')
+	img.save(xkcdImgPath)
+
+getHeader()
 
 imgPath='/home/simena/household/xkcdDesktop/'
 getImage(imgPath)
 text=getMouseOverText()
-textToImage(text,imgPath + 'todaysXkcd.png')
-stitchImagesTogether(imgPath + 'todaysXkcd.png',imgPath + 'text.png')
+textImage=textToImage(text,imgPath,'todaysXkcd.png')
+stitchImagesTogether(imgPath + 'todaysXkcd.png',textImage)
 
 # set the comic as background
 os.system('gsettings set org.gnome.desktop.background picture-uri file://'+imgPath +  '/todaysXkcd.png')
